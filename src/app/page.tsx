13489ea418f3b66cbea621b22e2f514e444e0bde
GuilhermeMeduza <135604196+GuilhermeMeduza@users.noreply.github.com>
2025-09-15 17:44:45 -0300

    Mobile First Static Coming Soon Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,15 @@ import Logo from "../../public/Logo_Alves_E_Pimentel.svg";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-full dark:text-gray-light">
-      <Image src={Logo} width={500} alt="Logo Alves e Pimentel" />
-      <h1 className="text-5xl mt-18 font-lato font-light">Em Breve</h1>
+      <Image
+        src={Logo}
+        width={500}
+        className="min-[320px]:w-1/2 min-[720px]:w-3/8"
+        alt="Logo Alves e Pimentel"
+      />
+      <h1 className="min-[320px]:text-2xl md:text-5xl mt-18 font-lato font-light">
+        Em Breve
+      </h1>
     </div>
   );
 }
