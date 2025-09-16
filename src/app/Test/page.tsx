0742bfab9e8ae4c6daa3d6c918c30bea5930f8d7
GuilhermeMeduza <135604196+GuilhermeMeduza@users.noreply.github.com>
2025-09-16 08:58:07 -0300

    Add: Test Page

---
+++ src/app/Test/page.tsx
@@ -0,0 +1,9 @@
+export default function Test() {
+  return (
+    <div className="flex flex-col justify-center items-center h-full text-gray-light">
+      <h1 className="min-[320px]:text-2xl md:text-5xl mt-18 font-lato font-light">
+        Teste
+      </h1>
+    </div>
+  );
+}
